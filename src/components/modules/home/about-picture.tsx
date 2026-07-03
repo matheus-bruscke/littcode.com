41b9dbc17aee84e0453408c099a8666a21dcf6b6
Matheus Bruscke <matheus.bruscke@gmail.com>
2026-07-03 15:18:53 -0400

    refactor: decreasing picture size

--- src/components/modules/home/about-picture.tsx
+++ src/components/modules/home/about-picture.tsx
@@ -9,19 +9,21 @@ type AboutPictureProps = {
 
 function AboutPicture({ image }: AboutPictureProps) {
 	return (
-		<figure className="h-20 w-full hidden md:col-span-4 md:block md:border-l md:h-full border-ds-border border-dashed relative">
-			<Image
-				src={image}
-				alt="Matheus Bruscke"
-				fill
-				loading="eager"
-				sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-				className="object-cover object-center"
-			/>
+		<div className="flex-col hidden md:flex relative justify-end md:border-l md:h-full border-ds-border border-dashed md:col-span-4">
+			<figure className="h-[400px] w-full relative">
+				<Image
+					src={image}
+					alt="Matheus Bruscke"
+					fill
+					loading="eager"
+					sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+					className="object-cover object-top"
+				/>
+			</figure>
 			<code className="text-label-12 text-ds-border hover:text-ds-accent uppercase font-pixel [writing-mode:vertical-lr] absolute top-4 right-4">
 				Matheus Bruscke
 			</code>
-		</figure>
+		</div>
 	);
 }
 
